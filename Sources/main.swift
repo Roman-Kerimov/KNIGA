f046f34e63475c954b764a64307dd131d00a181e
Roman Kerimov <roma.kerimov@icloud.com>
2024-06-08 14:45:31 -0500

    vault backup: 2024-06-08 14:45:31

--- Sources/main.swift
+++ Sources/main.swift
@@ -35,6 +35,8 @@ do {
                 .resourceValues(forKeys: [.contentModificationDateKey])
                 .contentModificationDate!
             
+            print(targetModificationDate)
+            
             if sourceModificationDate > targetModificationDate {
                 let sourceText = try String(contentsOf: sourceURL)
                 

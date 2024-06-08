5c4b5fbac62249a29287157a934968a6b33a2ea2
Roman Kerimov <roma.kerimov@icloud.com>
2024-06-08 09:28:45 -0500

    vault backup: 2024-06-08 09:28:45

--- Sources/main.swift
+++ Sources/main.swift
@@ -4,4 +4,4 @@
 import Foundation
 import LinguisticKit
 
-print(FileManager.default.currentDirectoryPath)
+print(FileManager.default.url(for: ., in: <#T##FileManager.SearchPathDomainMask#>, appropriateFor: <#T##URL?#>, create: <#T##Bool#>).currentDirectoryPath)

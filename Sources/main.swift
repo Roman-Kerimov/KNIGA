decd2295c881e0aa1686e2cbb3b6ca81446d91f2
Roman Kerimov <roma.kerimov@icloud.com>
2024-06-08 16:41:53 -0500

    vault backup: 2024-06-08 16:41:53

--- Sources/main.swift
+++ Sources/main.swift
@@ -86,6 +86,7 @@ do {
                     }
                 }
                 .joined(separator: " | ")
+                .uppercased()
             
             try """
             ## \(scriptPicker)
